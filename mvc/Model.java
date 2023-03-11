92d39f189bf63b98863674ca22ba6697679bc1ae
Owen <104656426+onskys@users.noreply.github.com>
2023-03-11 13:32:16 -0800

    Added instance variables and methods.

--- mvc/Model.java
+++ mvc/Model.java
@@ -4,7 +4,37 @@ package mvc;
 
 3/10/2023 - Niko Jokhadze: Created file
 
+3/11/2023 - Owen Semersky: Added instance variables, getters, setters, and "changed" method.
+
  */
 
-public class Model {
+public class Model extends Bean {
+    private Boolean unsavedChanges;
+    private String fileName;
+
+    {
+        unsavedChanges = false;
+        fileName = null;
+    }
+
+    public void changed() {
+        unsavedChanges = true;
+        firePropertyChange(fileName, false, true);
+    }
+
+    public Boolean getUnsavedChanges() {
+        return unsavedChanges;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setUnsavedChanges(Boolean change) {
+        unsavedChanges = change;
+    }
+
+    public void setFileName(String newName) {
+        fileName = newName;
+    }
 }
