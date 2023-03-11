7bef568f83d442b5eb7b37e5e7bc4485daea8a03
Owen <104656426+onskys@users.noreply.github.com>
2023-03-11 12:23:59 -0800

    Added interface methods.

--- mvc/AppFactory.java
+++ mvc/AppFactory.java
@@ -4,7 +4,22 @@ package mvc;
 
 3/10/2023 - Niko Jokhadze: Created file
 
+3/11/2023 - Owen Semersky: Changed file from a class to interface, added interface methods.
+
  */
 
-public class AppFactory {
+public interface AppFactory {
+    public Model makeModel();
+
+    public View makeView();
+
+    public String getTitle();
+
+    public String getHelp();
+
+    public String about();
+
+    public String getEditCommands();
+
+    public Command makeEditCommand(String name);
 }
