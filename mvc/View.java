a896d97eda8fbe6647b1d5f4349386f5c2dde4cb
Hkumasu <116594262+Hkumasu@users.noreply.github.com>
2023-03-11 21:20:14 -0800

    Update View.java

--- mvc/View.java
+++ mvc/View.java
@@ -1,10 +1,35 @@
 package mvc;
 
+
+import javax.swing.*;
+import javax.swing.border.Border;
+import java.awt.*;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+
 /* Class "View" Datalog
-
 3/10/2023 - Niko Jokhadze: Created file
-
+3/11/2023 - Hazuki Sugahara: edit file
  */
+public class View extends JPanel implements PropertyChangeListener {
+    Model model;
 
-public class View {
+    public View(Model model){
+        this.model = model;
+        setSize(500, 300);
+        Border blackline = BorderFactory.createLineBorder(Color.black);
+        setBorder(blackline);
+        setBackground(Color.WHITE);
+    }
+
+    @Override
+    public void propertyChange(PropertyChangeEvent evt){
+
+        String event = evt.getPropertyName();
+
+        if("New".equals(event) || "Open".equals(event)){
+            Model oldModel = (Model)evt.getOldValue();
+            Model newModel = (Model) evt.getNewValue();
+        }
+    }
 }
