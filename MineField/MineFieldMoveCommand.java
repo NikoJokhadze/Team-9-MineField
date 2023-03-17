cd587de8e2f19f6b8be1b70c4310b9c91ffea144
Hkumasu <116594262+Hkumasu@users.noreply.github.com>
2023-03-17 13:51:13 -0700

    Update MineFieldMoveCommand.java

--- MineField/MineFieldMoveCommand.java
+++ MineField/MineFieldMoveCommand.java
@@ -1,10 +1,22 @@
 package MineField;
 
+import mvc.*;
 /* Class "MineField" Datalog
 
 3/16/2023 - Niko Jokhadze: Created file
+3/17/2023 - Hazuki Sugahara: Edited file
 
  */
 
-public class MineFieldMoveCommand {
+public class MineFieldMoveCommand extend Command{
+  Heading heading;
+  
+  public MineFieldMoveCommand(Model model) {
+    super(model);
+  }
+  
+  public void execute() {
+    MineField mineField= (MineField)model;
+    
+  }
 }
