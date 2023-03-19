a077842b855bd2f7e0a0b018fb0104ccf75ff61d
Owen <104656426+onskys@users.noreply.github.com>
2023-03-19 13:24:52 -0700

    Added try/catch for move exceptions.

--- MineField/MineFieldMoveCommand.java
+++ MineField/MineFieldMoveCommand.java
@@ -1,23 +1,27 @@
 package MineField;
 
 import mvc.*;
-/* Class "MineField" Datalog
 
+/* Class "MineField" Datalog
 3/16/2023 - Niko Jokhadze: Created file
 3/17/2023 - Hazuki Sugahara: Edited file
-
+3/19/2023 - Owen Semersky: Added try/catch for execute
  */
 
-public class MineFieldMoveCommand extends Command{
-  Heading heading;
-  
-  public MineFieldMoveCommand(Model model, Heading heading) {
-    super(model);
-    this.heading = heading;
-  }
-  
-  public void execute() {
-    MineField mine= (MineField)model;
-    m.move();
-  }
+public class MineFieldMoveCommand extends Command {
+    Heading heading;
+
+    public MineFieldMoveCommand(Model model, Heading heading) {
+        super(model);
+        this.heading = heading;
+    }
+
+    public void execute() {
+        MineField m = (MineField) model;
+        try {
+            m.move(heading);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
