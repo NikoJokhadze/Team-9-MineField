d09624b9a2049c1c3a34e2dc8b12560536b026be
Hkumasu <116594262+Hkumasu@users.noreply.github.com>
2023-03-18 19:59:27 -0700

    Update MineFieldPanel.java

--- MineField/MineFieldPanel.java
+++ MineField/MineFieldPanel.java
@@ -1,5 +1,7 @@
 package MineField;
 
+import mvc.*;
+import javax.swing.*;
 /* Class "MineField" Datalog
 
 3/16/2023 - Niko Jokhadze: Created file
@@ -8,15 +10,53 @@ package MineField;
  */
 
 public class MineFieldPanel extends AppPanel {
-  private JButton northWest;
-  private JButton north;
-  private JButton northEast;
-  private JButton west;
-  private JButton east;
-  private JButton southWest;
-  private JButton south;
-  private JButton southEast;
-  
-  public MineFieldPanel(AppFactory factory)
-  
+    private JButton northWest;
+    private JButton north;
+    private JButton northEast;
+    private JButton west;
+    private JButton east;
+    private JButton southWest;
+    private JButton south;
+    private JButton southEast;
+
+    public MineFieldPanel(AppFactory factory) {
+        super(factory);
+        northWest = new JButton("NW");
+        northWest.addActionListener(this);
+        addControl(northWest);
+
+        north = new JButton("N");
+        north.addActionListener(this);
+        addControl(north);
+
+        northEast = new JButton("NW");
+        northEast.addActionListener(this);
+        addControl(northEast);
+
+        west = new JButton("W");
+        west.addActionListener(this);
+        addControl(west);
+
+        east = new JButton("E");
+        east.addActionListener(this);
+        addControl(east);
+
+        southWest = new JButton("SW");
+        southWest.addActionListener(this);
+        addControl(southWest);
+
+        south = new JButton("S");
+        south.addActionListener(this);
+        addControl(south);
+
+        southEast = new JButton("SE");
+        southEast.addActionListener(this);
+        addControl(southEast);
+    }
+
+    public static void mian(String[] args) {
+        AppFactory factory = new MineFieldFactory();
+        AppPanel panel = new MineFieldPanel(factory);
+        panel.display();
+    }
 }
