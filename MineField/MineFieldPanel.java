f0c120dd397158eda9f07ceeb4b1967d3647e159
Hkumasu <116594262+Hkumasu@users.noreply.github.com>
2023-03-18 18:27:39 -0700

    Update MineFieldPanel.java

--- MineField/MineFieldPanel.java
+++ MineField/MineFieldPanel.java
@@ -3,8 +3,18 @@ package MineField;
 /* Class "MineField" Datalog
 
 3/16/2023 - Niko Jokhadze: Created file
+3/18/2023 - Hazuki Sugahara: Edited file
 
  */
 
-public class MineFieldPanel {
+public class MineFieldPanel extends AppPanel {
+  private JButton northWest;
+  private JButton north;
+  private JButton northEast;
+  private JButton west;
+  private JButton east;
+  private JButton southWest;
+  private JButton south;
+  private JButton southEast;
+  
 }
